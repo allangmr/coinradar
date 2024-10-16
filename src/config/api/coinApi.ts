a2e05ddc30612b3a61dec516192b372f9be53a53
Allan Montilla <allangmr7@gmail.com>
2024-10-16 18:13:29 -0500

    Add: TanStack Library and Configurations

---
+++ src/config/api/coinApi.ts
@@ -0,0 +1,9 @@
+import { THE_COINGECKO_DB_KEY } from "@env";
+import axios from 'axios';
+
+export const coinApi = axios.create({
+  baseURL: 'https://api.coingecko.com/api/v3',
+    params: {
+        x_cg_demo_api_key: THE_COINGECKO_DB_KEY ?? 'no-key',
+    },
+});
